cd73d82cd8b20ee7d0ae52d6c06e804c51c60fe6
ardalan@mobilefringe.com @ CodeCloud <ardalan@mobilefringe.com @ CodeCloud>
2016-05-27 16:28:49 -0400

    Riverside Plaza 1.0  /site.js

---
+++ site.js
@@ -0,0 +1,6 @@
+var site_json = {
+                    "name" : "Riverside Plaza",
+                    "default_image" : "http://assets.codecloudapp.com/sites/574893336e6f647986010000/image/jpeg/1463757854000/dgvr_500x500_default.jpg",
+                    "time_zone" : "T08:00:00Z",
+                    "instagram" : "196558634"
+                }
