a3b9143fd741325b83fc84c181be3ab8145f7b62
rajbir@mobilefringe.com @ CodeCloud <rajbir@mobilefringe.com @ CodeCloud>
2016-06-13 13:44:44 -0400

    Riverside Plaza 1.0  /site.js

--- site.js
+++ site.js
@@ -2,5 +2,5 @@ var site_json = {
                     "name" : "Riverside Plaza",
                     "default_image" : "http://assets.codecloudapp.com/sites/574893336e6f647986010000/image/jpeg/undefined/rp_500x500_logo.jpg",
                     "time_zone" : "T08:00:00Z",
-                    "social_feed": "http://costamesa.mallmaverick.com/api/v2/costamesa/social.json"
+                    "social_feed": "http://riverside.mallmaverick.com/api/v2/riverside/social.json"
                 }
