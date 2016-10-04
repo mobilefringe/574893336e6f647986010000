da69fc1e8971d510e0205b6c0f5fac93f7517db9
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2016-10-04 16:32:46 -0400

    Riverside Plaza 1.1  /site.js

---
+++ site.js
@@ -0,0 +1,6 @@
+var site_json = {
+                    "name" : "Riverside Plaza",
+                    "default_image" : "//codecloud.cdn.speedyrails.net/sites/57f40f956e6f6439594d0000/image/jpeg/undefined/rp_500x500_logo.jpg",
+                    "time_zone" : "T08:00:00Z",
+                    "social_feed": "//riverside.mallmaverick.com/api/v2/riverside/social.json"
+                }
